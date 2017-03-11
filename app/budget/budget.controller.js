a4b36e7eaecaca665e68b9611de71634c793e5e1
Arlin <arlin.grijalba@gmail.com>
2017-03-10 23:36:52 -0800

    Created functions to check expenses. (Not finished)

--- app/budget/budget.controller.js
+++ app/budget/budget.controller.js
@@ -17,6 +17,12 @@ function BudgetController() {
     vm.totalExpense = 0;
     vm.totalLeft = 0;
 
+    //words to check
+    vm.rentWordsArray = ["House", "Rent", "Mortgage"];
+
+    ////// functions
+
+    //// add
     vm.addIncome = function() {
         vm.incomeArray.push({
             description: vm.incomeDescription,
@@ -37,8 +43,23 @@ function BudgetController() {
         vm.expenseAmount = '';
         vm.calculateExpense();
         vm.calculateTotalLeft();
+        vm.checkExpenses();
     }
 
+    ////deletes
+    vm.deleteIncome = function(index) {
+        vm.incomeArray.splice(index, 1);
+        calculateIncome();
+        calculateTotalLeft();
+    }
+
+    vm.deleteExpense = function(index) {
+        vm.expenseArray.splice(index, 1);
+        calculateExpense();
+        calculateTotalLeft();
+    }
+
+    //// calculations
     vm.calculateIncome = function() {
         vm.totalIncome = vm.incomeArray.reduce(function(a, b) {
             return a + b.amount;
@@ -55,15 +76,13 @@ function BudgetController() {
         vm.totalLeft = vm.totalIncome - vm.totalExpense;
     }
 
-    vm.deleteIncome = function(index){
-      vm.incomeArray.splice(index, 1);
-      calculateIncome();
-      calculateTotalLeft();
-    }
+    vm.checkExpenses = function() {
+        for (var i = 0; i < rentWordsArray.length; i++) {
+            if (vm.expenseArray[expenseArray.length - 1].indexOf(rentWordsArray[i])) {
+              console.log("Si hay: " rentWordsArray[i]);
+              break;
+            }
+        }
 
-    vm.deleteExpense = function(index){
-      vm.expenseArray.splice(index, 1);
-      calculateExpense();
-      calculateTotalLeft();
     }
 }
